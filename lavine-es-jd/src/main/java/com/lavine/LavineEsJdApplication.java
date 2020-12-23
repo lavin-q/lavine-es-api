b2ae14042b8a7a334e8c706b00bfdfd4dec48b2a
qihumao <915940173@qq.com>
2020-12-23 10:49:02 +0800

    jd 爬虫实战

---
+++ lavine-es-jd/src/main/java/com/lavine/LavineEsJdApplication.java
@@ -0,0 +1,13 @@
+package com.lavine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavineEsJdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavineEsJdApplication.class, args);
+	}
+
+}
---
+++ lavine-es-jd/src/test/java/com/lavine/LavineEsJdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lavine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavineEsJdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
